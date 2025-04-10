56349d62fe0aa8ff25f62d2a779a4235967ebc5a
Maico Vivas <159493021+Mav233@users.noreply.github.com>
2025-04-09 21:18:40 -0300

    Clase-03
    Clase de java

---
+++ Clase03/src/main/java/ArraysClase03.java
@@ -0,0 +1,118 @@
+//import java.util.ArrayList;
+//import java.util.List;
+//
+//public class ArraysClase03 {
+//
+//	public static void main(String[] args) {
+	
+		/*int [] numerosEnteros = new int [5]; //defino la cantidad total de elementos
+		numerosEnteros [0] = 5;
+		numerosEnteros [0] = 15; //estoy reasignando
+		numerosEnteros [1] = 10;
+		numerosEnteros [2] = 20;
+		numerosEnteros [3] = 30;
+		numerosEnteros [4] = 40;
+		//numerosEnteros [5] = 50; //6
+	
+		
+		System.out.println("Posicion en memoria del array" + numerosEnteros);
+		
+		System.out.println("el elemento en la posicion 0 es: " + numerosEnteros[0]);
+		System.out.println("el elemento en la posicion 1 es: " + numerosEnteros[1]);
+		System.out.println("el elemento en la posicion 2 es: " + numerosEnteros[2]);
+		System.out.println("el elemento en la posicion 3 es: " + numerosEnteros[3]);
+		System.out.println("el elemento en la posicion 4 es: " + numerosEnteros[4]);
+		
+		
+		//en javascript las llaves se refieren a un objeto, en cambio en java se refiere a un bloque.
+		
+		String[] palabras = {
+				"hola",
+				"mundo",
+				"codigo",
+				"maico",
+				"lenovo",
+				"otra palabra"
+		};
+		
+		System.out.println(" el array de palabras tiene " + palabras.length + " elementos");
+		
+		System.out.println(Arrays.toString(palabras));*/
+		
+		
+		//LISTAS
+		
+		
+//		List<String> palabras = new ArrayList<>();
+//		List<String> palabras2 = new ArrayList<>();
+//		
+//		palabras.add("hola");
+//		palabras.add("maico");
+//		palabras.add("como");
+//		palabras.add("esta");
+//		palabras.add("todo");
+//		palabras.remove("bien");
+//		
+//		palabras2.add("agustin");
+//		palabras2.add("cordoba");
+//		palabras2.add("argentina");
+		
+//		System.out.println(palabras);
+//		System.out.println("La lista de palabras tiene: " + palabras.size() + "elementos");
+
+//		palabras.addAll(palabras2); // para unir dos listas o mas
+		
+		//System.out.println(palabras);
+		
+		//System.out.println(palabras.containsAll(palabras2));
+		
+		//palabras.removeAll(palabras2);
+		//System.out.println(palabras);
+		//System.out.println(palabras.containsAll(palabras2));
+		
+		
+		//palabras.clear();
+		//System.out.println(palabras);
+		
+		
+//		if(palabras.isEmpty()) {
+//			System.out.println("La lista palabras esta vacia !");
+//		} else {
+////			for (String palabra : palabras) {
+////			System.out.println("La palabra con Indice: " + palabra);
+////			}
+//			
+//			for (int i = 0; i < palabras.size(); i++) {
+//				System.out.println("La palabra con Indice: " + i + " es: " + palabras.get(i));
+//			}
+//		}
+		
+		//idexOf se utiliza para encontrar la posicion de un elemento, en una lista. 
+
+//		int indice = palabras.indexOf("como");
+//		System.out.println("El indice de la palabra -como- es : " + indice);
+//		
+		
+		// WHILE Y DO WHILE
+		//int indice = 0;
+		
+//		while(!palabras.isEmpty() && indice < palabras.size()) {
+//			System.out.println("Palabra : " + palabras.get(indice));
+//			indice++;
+//		}
+		
+//		do {
+//			System.out.println("Palabra = " + palabras.get(indice) );
+//			indice++;
+//		} while (indice < palabras.size());
+		
+		
+		//OTRA FORMA DE DECLARAR FOREACH
+		
+//		palabras.forEach( palabra -> {
+//			System.out.println("Palabra : " + palabra);
+//		});
+		
+//	}
+//
+//}
