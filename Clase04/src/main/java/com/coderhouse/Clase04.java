a1601a697d336a22067475835a51fccdacc878fb
Maico Vivas <159493021+Mav233@users.noreply.github.com>
2025-04-12 09:42:34 -0300

    Clase04

---
+++ Clase04/src/main/java/com/coderhouse/Clase04.java
@@ -0,0 +1,56 @@
+//package com.coderhouse;
+//
+//import com.coderhouse.objetos.Perros;
+//
+//public class Clase04 {
+//
+//	public static void main(String[] args) {
+
+//		Perros segundoPerro = new Perros();
+//        /*
+//		primerPerro.nombre = "Firulay";
+//		primerPerro.raza = "caniche";
+//		primerPerro.color = "negro";
+//		primerPerro.tamanio= "mediano";
+//		primerPerro.edad= 4;
+//		primerPerro.peso= 4.5;
+//		
+//		
+//		System.out.println(primerPerro.nombre);
+//		System.out.println(primerPerro.raza);
+//		System.out.println(primerPerro.color);
+//		System.out.println(primerPerro.tamanio);
+//		System.out.println(primerPerro.edad);
+//		System.out.println(primerPerro.peso);
+//		*/
+//		primerPerro.ladrar();
+//		primerPerro.caminar();
+//		primerPerro.comer();
+//		
+//		primerPerro.setNombre("firulay");
+//		primerPerro.setRaza("Caniche");
+//		primerPerro.setColor("negro");
+//		primerPerro.setTamanio("mediano");
+//		primerPerro.setEdad(4);
+//		primerPerro.setPeso(4.5);
+//		
+//		System.out.println("Mi perro " + primerPerro.getNombre() + " es un " + primerPerro.getRaza() + " de color " + primerPerro.getColor());
+
+//		Perros segundoPerro = new Perros();
+//		
+//		segundoPerro.setNombre("juanjo");
+//		segundoPerro.setRaza("labrador");
+//		segundoPerro.setColor("blanco");
+//		segundoPerro.setTamanio("grande");
+//		segundoPerro.setEdad(6);
+//		segundoPerro.setPeso(14.5);
+//		
+//		
+//		segundoPerro.mostrarAtributos();
+//		
+//		
+//		System.out.println(segundoPerro);
+		
+//	}
+//
+//}
---
+++ Clase04/src/main/java/com/coderhouse/objetos/Perros.java
@@ -0,0 +1,96 @@
+//package com.coderhouse.objetos;
+//
+//public class Perros {
+//	
+//	
+//	//ATRIBUTOS DEL OBJETO. O VARIABLES DE INSTANCIA.
+//	
+//	public String nombre;
+//	public String raza;
+//	public String color;
+//	public String tamanio;
+//	public int edad;
+//	public double peso;
+//	
+//	
+//	
+//	//METODOS (son lo que podria hacer el objeto, lo que hace el perro en este caso)
+//	
+//	public void ladrar() {
+//		System.out.println("el perro esta ladrando");
+//	}
+//	
+//	public void caminar() {
+//		System.out.println("el perro esta caminando");
+//	}
+//	
+//	public void comer() {
+//		System.out.println("el perro esta comiendo");
+//	}
+//
+//	
+//	
+//	//ECAPSULAMIENTO
+//	
+//	//GETTERS Y SETTERS
+//	
+//	
+//	public String getNombre() {
+//		return nombre;
+//	}
+//
+//	public void setNombre(String nombre) {
+//		this.nombre = nombre;
+//	}
+//
+//	public String getRaza() {
+//		return raza;
+//	}
+//
+//	public void setRaza(String raza) {
+//		this.raza = raza;
+//	}
+//
+//	public String getColor() {
+//		return color;
+//	}
+//
+//	public void setColor(String color) {
+//		this.color = color;
+//	}
+//
+//	public String getTamanio() {
+//		return tamanio;
+//	}
+//
+//	public void setTamanio(String tamanio) {
+//		this.tamanio = tamanio;
+//	}
+//
+//	public int getEdad() {
+//		return edad;
+//	}
+//
+//	public void setEdad(int edad) {
+//		this.edad = edad;
+//	}
+//
+//	public double getPeso() {
+//		return peso;
+//	}
+//
+//	public void setPeso(double peso) {
+//		this.peso = peso;
+//	}
+//	
+//	public void mostrarAtributos() {
+//		System.out.println("Mi perro " + this.getNombre() + " es un " + this.getRaza() + " de color " + this.getColor());
+//		}
+//
+//	@Override
+//	public String toString() {
+//		return "Perros [nombre=" + nombre + ", raza=" + raza + ", color=" + color + ", tamanio=" + tamanio + ", edad="
+//				+ edad + ", peso=" + peso + "]";
+//	}
+
+//}
